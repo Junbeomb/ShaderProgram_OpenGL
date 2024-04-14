b832fd78cdda1f6f8060a269a0092ef2eb315397
Junbeomb <junbeomb216@gmail.com>
2024-04-15 00:07:24 +0900

    Draw circle FSSnadbox.fs

--- SimpleGame/Shaders/FSSandbox.fs
+++ SimpleGame/Shaders/FSSandbox.fs
@@ -2,7 +2,20 @@
 
 layout(location=0) out vec4 FragColor;
 
+in vec4 v_Color;
 void main()
 {
-	FragColor = vec4(1,1,1,1);
+	vec4 newColor = vec4(1,1,1,1);
+	float r = 0.5;
+	vec2 center = vec2(0.5,0.5);
+	float dist = distance(v_Color.rg,center);
+
+	if(dist < r){
+		newColor = vec4(1,1,1,1);
+	}
+	else {
+		newColor = vec4(0,1,0,0);
+	}
+
+	FragColor = newColor;
 }
